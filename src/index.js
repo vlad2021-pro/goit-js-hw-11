4c467fdb43b62903b312e9c2555199090aa8cf6f
vlad2021-pro <84739712+vlad2021-pro@users.noreply.github.com>
2021-11-24 22:33:09 +0300

    внес правки

--- src/index.js
+++ src/index.js
@@ -1,96 +1,168 @@
-// import './css/styles.css';
-import Notiflix from 'notiflix'
-import { getPictures } from './aip.axios'
+import './sass/main.scss';
+import { Notify } from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css';
+import Simplelightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
-
-const searchForm = document.querySelector('#search-form')
-const loadMoreBtn = document.querySelector('.load-more')
-const galleryContainer = document.querySelector('.gallery')
-
-let searchPictures = ""
-let page = 1;
-let PicturesLimit = 40
-let totalHits
-
-searchForm.addEventListener('submit', onSubmit)
-loadMoreBtn.addEventListener('click', loadMore)
-
-function submitCreateCard(image) {
-
-    const dataCardImages = image.data.hits;
-    
-    if (dataCardImages.length === 0) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-    }
-    else {
-        galleryContainer.innerHTML = dataCardImages.reduce((imagesList, { webformatURL, tags, likes, views, comments, downloads }) => {
-            const template = `<div class="photo-card">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy width=60px" />
-                <div class="info">
-                    <p class="info-item">
-                    <b>Likes: <span class=info-item-color>${likes}</span></b>
-                    </p>
-                    <p class="info-item">
-                    <b>Views: <span class=info-item-color>${views}</span></b>
-                    </p>
-                    <p class="info-item">
-                    <b>Comments: <span class=info-item-color>${comments}</span></b>
-                    </p>
-                    <p class="info-item">
-                    <b>Downloads: <span class=info-item-color>${downloads}</span></b>
-                    </p>
-                </div>
-                </div>`
-            return imagesList + template
-        }, '')
-    }
-}
-
-function loadMoreCreateCard(image) {
-    const dataCardImages = image.data.hits
-    totalHits = image.data.totalHits
-    
-    dataCardImages.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
-       const string = `<div class="photo-card">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
-                <div class="info">
-                    <p class="info-item">
-                    <b>Likes: <span class=info-item-color>${likes}</span></b>
-                    </p>
-                    <p class="info-item">
-                    <b>Views: <span class=info-item-color>${views}</span></b>
-                    </p>
-                    <p class="info-item">
-                    <b>Comments: <span class=info-item-color>${comments}</span></b>
-                    </p>
-                    <p class="info-item">
-                    <b>Downloads: <span class=info-item-color>${downloads}</span></b>
-                    </p>
-                </div>
-                </div>`
-        imagesContainer.insertAdjacentHTML('beforeend', string)
-    })
-}
-    async function loadMore() {
-    if (page * PicturesLimit >= totalHits) {
-        loadMoreBtn.classList.add('is-hidden');
-        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
-    } else {
-        page += 1;
-        const images = await getPictures(searchPictures, page, PicturesLimit)
-        loadMoreCreateCard(images);
-    }
-}
-
-async function onSubmit(event) {
-    event.preventDefault();
-    page = 1;
-
-    loadMoreBtn.classList.add('is-hidden');
-    searchPictures = searchForm.searchQuery.value.trim();
-
-    const images = await getPictures(searchPictures, page, PicturesLimit);
-submitCreateCard(images);
-
-   loadMoreBtn.classList.remove('is-hidden');
+const refs = {
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('.search-form__input'),
+  loadMoreButton: document.querySelector('.load-more'),
+  galleryList: document.querySelector('.gallery'),
 };
+
+
+const lightBoxList = {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  showCounter: true,
+};
+
+const BASE_URL = 'https://pixabay.com/api/';
+const searchOptions = {
+  key: '11408941-3cf7894bd0fa3b9fec7ed7cf5',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+  page: 1,
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreButton.addEventListener('click', onClickButtonLoadMore);
+
+let gallery = new Simplelightbox('.gallery a', lightBoxList);
+
+function onClick(evt) {
+  evt.preventDefault();
+  gallery.open('.gallery');
+}
+
+async function onFormSubmit(evt) {
+  evt.preventDefault();
+  searchOptions.page = 0;
+  searchOptions.q = evt.currentTarget.elements.searchQuery.value;
+
+  if (searchOptions.q === '') {
+    showInfoNotification();
+    clearPreviousRequest();
+    return;
+  }
+
+  try {
+    const collection = await onFetchCollection();
+    successfulFunctionExecution(collection);
+  } catch (error) {
+    showFailureNotification();
+  }
+  refs.form.reset();
+}
+
+function successfulFunctionExecution(answer) {
+  clearPreviousRequest();
+  renderMarkup(answer);
+  refs.galleryList.addEventListener('click', onClick);
+  gallery.refresh();
+  showSuccessNotification(answer);
+}
+
+async function onFetchCollection() {
+  searchOptions.page += 1;
+  const searchParams = { params: searchOptions };
+  const response = await axios.get(BASE_URL, searchParams);
+
+  if (response.data.hits.length === 0) {
+    throw new Error('Whoops');
+  }
+  return response;
+}
+
+async function onClickButtonLoadMore() {
+  try {
+    const collection = await onFetchCollection();
+    renderMarkup(collection);
+    gallery.refresh();
+    smoothScrolling();
+  } catch (error) {
+    errorNotificationEndOfRequest();
+    refs.loadMoreButton.classList.add('visually-hidden');
+  }
+}
+
+function clearPreviousRequest() {
+  refs.galleryList.removeEventListener('click', onClick);
+  refs.galleryList.innerHTML = '';
+  refs.loadMoreButton.classList.add('visually-hidden');
+}
+
+function smoothScrolling() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function renderMarkup({ data }) {
+  const markup = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+  <a href="${largeImageURL}" class="photo-link" >
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" class="photo-img"/>
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${likes}
+    </p>
+    
+    <p class="info-item">
+      <b>Views</b> ${views}
+    </p>
+    
+    <p class="info-item">
+      <b>Comments</b> ${comments}
+    </p>
+    
+    <p class="info-item">
+      <b>Downloads</b> ${downloads}
+    </p>
+    <i class="bi bi-download"></i>
+    
+  </div>
+</div>`,
+    )
+    .join('');
+
+  refs.galleryList.insertAdjacentHTML('beforeend', markup);
+  refs.loadMoreButton.classList.remove('visually-hidden');
+}
+
+function errorNotificationEndOfRequest() {
+  Notify.failure("We're sorry, but you've reached the end of search results.");
+}
+
+function showFailureNotification() {
+  Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+
+function showSuccessNotification(answer) {
+  Notify.success(`Hooray! We found ${answer.data.totalHits} images.`);
+}
+
+function showInfoNotification() {
+  Notify.info("The search term couldn't be empty.");
+}
